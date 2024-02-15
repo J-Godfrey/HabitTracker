8a9e2f99f450cdc8ec164383b450000088d5da67
j-Godfrey <j.godfrey2142@gmail.com>
2024-02-15 21:17:40 +1030

    Added SQLite functionality

--- HabitTracker/Program.cs
+++ HabitTracker/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string connectionString = @"Data Source=HabitTracker.db";
+
+
+        using (SqliteConnection connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+            SqliteCommand tablecmd = connection.CreateCommand();
+
+            tablecmd.CommandText = 
+                    @"CREATE TABLE IF NOT EXISTS drinking_water (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Date TEXT,
+                        Quantity INTEGER
+                        )";
+
+            tablecmd.ExecuteNonQuery();
+
+            connection.Close();
+        }
+
+
+    }
+}
